79d55f82bfffd5b6cd4956ee689f5565658dc2cd
AliSuliman <ali.sliman@ramaaz.com>
2024-07-09 09:43:48 +0300

    configure base server and add jasmine configuration

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.log('Hello World');
-let number;
+import express, { Request, Response } from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World !',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port:${PORT}`);
+});
+
+export default app;
